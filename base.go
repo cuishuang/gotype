26545db4c39cd44fc93c1ac8c6b4c6d62da3c3db
cuishuang <imcusg@gmail.com>
2020-07-19 23:28:22 +0800

    add

---
+++ base.go
@@ -0,0 +1,59 @@
+package gotype
+
+//链表定义
+type LNode struct {
+	Data interface{}
+	Next *LNode
+}
+
+//链表定义 这才是爽哥习惯的方式,也是leetcode默认的方式
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+
+func NewLNode() *LNode {
+	return &LNode{}
+}
+
+// 爽哥新增
+func NewLNodeInt() *ListNode {
+	return &ListNode{}
+}
+
+
+//二叉树定义
+type BNode struct {
+	Data       interface{}
+	LeftChild  *BNode
+	RightChild *BNode
+}
+
+func NewBNode() *BNode {
+	return &BNode{}
+}
+
+//Trie树
+type TrieNode struct {
+	IsLeaf bool
+	Url    string
+	Child  []*TrieNode
+}
+
+func NewTrieNode(count int) *TrieNode {
+	return &TrieNode{IsLeaf: false, Url: "", Child: make([]*TrieNode, count)}
+}
+
+//AVL树
+type AVLNode struct {
+	Data   int
+	Height int
+	Count  int
+	Left   *AVLNode
+	Right  *AVLNode
+}
+
+func NewAVLNode(data int) *AVLNode {
+	return &AVLNode{data, 1, 1, nil, nil}
+}
